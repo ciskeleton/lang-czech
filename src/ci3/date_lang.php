185de18871df1ce0c1d85f123718ba648b31d24d
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-09-26 10:23:02 +0100

    Squashing Commit!

---
+++ src/ci3/date_lang.php
@@ -0,0 +1,75 @@
+<?php
+
+$lang['date_year'] = 'Rok';
+$lang['date_years'] = 'Roky';
+$lang['date_month'] = 'Měsíc';
+$lang['date_months'] = 'Měsíce';
+$lang['date_week'] = 'Týden';
+$lang['date_weeks'] = 'Týdny';
+$lang['date_day'] = 'Den';
+$lang['date_days'] = 'Dny';
+$lang['date_hour'] = 'Hodina';
+$lang['date_hours'] = 'Hodiny';
+$lang['date_minute'] = 'Minuta';
+$lang['date_minutes'] = 'Minuty';
+$lang['date_second'] = 'Sekunda';
+$lang['date_seconds'] = 'Sekundy';
+
+$lang['date_just_now'] = 'právě teď';
+$lang['date_seconds_ago'] = '=1{před # sekundou} 2-4{před # sekundami} other{před # sekundami}';
+$lang['date_minutes_ago'] = '=1{před # minutou} 2-4{před # minutami} other{před # minutami}';
+$lang['date_hours_ago'] = '=1{před # hodinou} 2-4{před # hodinami} other{před # hodinami}';
+$lang['date_days_ago'] = '=1{včera} 2-4{před # dny} other{před # dny}';
+$lang['date_weeks_ago'] = '=1{před # týdnem} 2-4{před # týdny} other{před # týdny}';
+$lang['date_months_ago'] = '=1{před # měsícem} 2-4{před # měsíci} other{před # měsíci}';
+$lang['date_years_ago'] = '=1{před # rokem} 2-4{před # lety} other{před # lety}';
+
+$lang['date_su'] = 'Ne';
+$lang['date_mo'] = 'Po';
+$lang['date_tu'] = 'Út';
+$lang['date_we'] = 'St';
+$lang['date_th'] = 'Čt';
+$lang['date_fr'] = 'Pá';
+$lang['date_sa'] = 'So';
+
+$lang['date_sun'] = 'Ne';
+$lang['date_mon'] = 'Po';
+$lang['date_tue'] = 'Út';
+$lang['date_wed'] = 'St';
+$lang['date_thu'] = 'Čt';
+$lang['date_fri'] = 'Pá';
+$lang['date_sat'] = 'So';
+
+$lang['date_sunday'] = 'Neděle';
+$lang['date_monday'] = 'Pondělí';
+$lang['date_tuesday'] = 'Úterý';
+$lang['date_wednesday'] = 'Středa';
+$lang['date_thursday'] = 'Čtvrtek';
+$lang['date_friday'] = 'Pátek';
+$lang['date_saturday'] = 'Sobota';
+
+$lang['date_jan'] = 'Led';
+$lang['date_feb'] = 'Ún';
+$lang['date_mar'] = 'Bře';
+$lang['date_apr'] = 'Dub';
+$lang['date_may'] = 'Kvě';
+$lang['date_jun'] = 'Čer';
+$lang['date_jul'] = 'Čvnc';
+$lang['date_aug'] = 'Srp';
+$lang['date_sep'] = 'Zář';
+$lang['date_oct'] = 'Říj';
+$lang['date_nov'] = 'Lis';
+$lang['date_dec'] = 'Pro';
+
+$lang['date_january'] = 'Leden';
+$lang['date_february'] = 'Únor';
+$lang['date_march'] = 'Březen';
+$lang['date_april'] = 'Duben';
+$lang['date_mayl'] = 'Květen';
+$lang['date_june'] = 'Červen';
+$lang['date_july'] = 'Červenec';
+$lang['date_august'] = 'Srpen';
+$lang['date_september'] = 'Září';
+$lang['date_october'] = 'Říjen';
+$lang['date_november'] = 'Listopad';
+$lang['date_december'] = 'Prosinec';
---
+++ src/ci3/upload_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['upload_userfile_not_set'] = 'Nepodařilo se nalézt post proměnnou userfile.';
+$lang['upload_file_exceeds_limit'] = 'Nahraný soubor překračuje maximální povolenou velikost ve vašem konfiguračním souboru PHP.';
+$lang['upload_file_exceeds_form_limit'] = 'Překročena velikost souboru určená formulářem.';
+$lang['upload_file_partial'] = 'Soubor byl úspěšně nahrán.';
+$lang['upload_no_temp_directory'] = 'Chybí dočasný adresář.';
+$lang['upload_unable_to_write_file'] = 'Soubor se nepodařilo zapsat na disk.';
+$lang['upload_stopped_by_extension'] = 'Nahrávání souboru zastaveno rozšířením.';
+$lang['upload_no_file_selected'] = 'Nebyl zvolen soubor k nahrání.';
+$lang['upload_invalid_filetype'] = 'Tento typ souboru není dovoleno nahrát.';
+$lang['upload_invalid_filesize'] = 'Velikost souboru je větší než povolená.';
+$lang['upload_invalid_dimensions'] = 'Obrázek má jiné než povolené rozměry.';
+$lang['upload_destination_error'] = 'Nastal problém při přesouvání souboru do cílové lokace.';
+$lang['upload_no_file_types'] = 'Nejsou specifikovány žádné povolené typy souboru.';
+$lang['upload_bad_filename'] = 'Jméno souboru na serveru již existuje.';
+$lang['upload_not_writable'] = 'Do cesty pro upload se nepodařilo zapsat.';
